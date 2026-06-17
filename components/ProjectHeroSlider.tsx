08420f563c3a846165c7a831405953ce4f8b7238
1992land <ngothaitinh852@gmail.com>
2026-06-17 21:58:16 +0700

    style: increase desktop banner height +10% (552px → 607px)

--- components/ProjectHeroSlider.tsx
+++ components/ProjectHeroSlider.tsx
@@ -48,7 +48,7 @@ export default function ProjectHeroSlider({ images, title }: Props) {
 
   return (
     <div
-      className="relative w-full h-[276px] sm:h-[437px] lg:h-[552px] overflow-hidden bg-navy-950 select-none"
+      className="relative w-full h-[276px] sm:h-[437px] lg:h-[607px] overflow-hidden bg-navy-950 select-none"
       style={{ touchAction: "pan-y" }}
       onMouseEnter={() => setPaused(true)}
       onMouseLeave={() => setPaused(false)}
